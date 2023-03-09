1acb7a8792d59c81146d5a2f3a0eb3d8f154ff8d
dhkim-dong <84676662+dhkim-dong@users.noreply.github.com>
2023-03-09 21:41:08 +0900

    third commit
    backjunt 입출력 문제 완. 조건문 시작

--- C++ Project/C++ Project/backjun1_2.cpp
+++ C++ Project/C++ Project/backjun1_2.cpp
@@ -20,11 +20,16 @@
 
 // std라는 네임스페이스 사용을 줄이기 위해 선언한다.
 using namespace std;
-int main() {
+int main5151() {
 	int a, b;
 	cin >> a >> b;
 	cout << a + b;
+	return 0;
 }
 
-// return 0을 해야하는 이유?
+// return 0을 해야하는 이유? 현재 실행 중인 함수를 끝낸다는 의미이다.
+// OS의 쉘에서 0을 true로 간주하기 때문이다.
 // precision cpp에서 소수점을 고정시키는 메서드
+
+// a+b, a-b, a*b. a/b, a%b를 출력하는 프로그램
+// endl "\n" 의 역할 + 내부 버퍼를 비우는 기능.
---
+++ C++ Project/C++ Project/backjun1_4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main466() {
+	int a;
+	cin >> a;
+	cout << a - (2541 - 1998);
+	return 0;
+}
+
+// 입력 받은 값에서 2541을 입력하면 1998이 나오는 프로그래밍을 구현
